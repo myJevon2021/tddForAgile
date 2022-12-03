44e947bfecaf8d3b101ab7c59b8194f9141db753
chen.l.37 <chen.l.37@pg.com>
2022-12-03 17:46:46 +0800

    1203将本地项目上传github

---
+++ src/main/java/com/goodwe/tddForAgile/TddForAgileApplication.java
@@ -0,0 +1,13 @@
+package com.goodwe.tddForAgile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddForAgileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddForAgileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/goodwe/tddForAgile/TddForAgileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodwe.tddForAgile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddForAgileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
